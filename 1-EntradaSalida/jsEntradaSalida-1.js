c314870bf3abd987e0a0939ff569fa4fc69df8e6
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-06 11:13:13 -0300

    Ejercicio uno
    Aprendí bocha.

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,37 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+
+	alert("Esto funciona de maravilla");
+}	
+{
+	var primerPrecio
+    var segundoPrecio
+    var Apellido="nadie"
+    Apellido="perez";
+    alert("Apellido")
+    // es un valor literal si esta entre ""
+    //alert("tal cual");//literal
+    alert("Apellido");
+    alert(primerPrecio); 
+
+
 }
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
