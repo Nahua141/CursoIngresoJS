ddc1bedce2ad43765d0b86cd5a5c460fe8126db8
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-08 12:31:58 -0300

    Ejercicio 4
    Prompt y id.

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -1,9 +1,17 @@
 /* 
 	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+	alert= sale un cuadro con una alerta.
 */
 function mostrar()
-{
-
+{	
+	var nombre; 
+	
+	nombre=prompt("Ingrese su nombre:"); //el prompt nos muestra un mensaje.
+	//elNombre
+	document.getElementById("elNombre").value = nombre;
 }
+
+
+
 
