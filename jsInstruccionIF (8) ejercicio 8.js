e73d066aa2df2ecd52c72a107b5482c2b7f69f69
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-14 12:13:39 -0300

    Ejercicio 8 If
    IF

---
+++ jsInstruccionIF (8) ejercicio 8.js
@@ -0,0 +1,18 @@
+function mostrar()
+{
+	var edad;
+	var estadoCivil;
+
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+	estadoCivil=document.getElementById('estadoCivil').value;
+	
+
+	if(edad>17 && estadoCivil=="Soltero")
+	{
+		alert("Es soltero y no es menor");
+	}
+	
+
+}//FIN DE LA FUNCIÓN
