3625ff55b94567540b0e717837a9712df84e459d
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-15 11:49:29 -0300

    Ejercicio 3 If
    Ejercicio 3 resuelto

---
+++ jsInstruccionIF (3) ejercicio 3 2.0.js
@@ -0,0 +1,17 @@
+function mostrar()
+{
+	var edad;
+	edad=document.getElementById('edad').value; 
+	edad=parseInt(edad);
+	
+	if (edad>17)
+		{
+			alert("es mayor de edad");
+		}
+		else
+		{
+			alert("es menor de edad");
+		}
+		
+}
+//FIN DE LA FUNCIÓN
