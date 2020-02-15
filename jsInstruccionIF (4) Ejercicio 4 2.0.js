64a3cbb1d56d76f2cb7ed5beefa2c4a2e3fc2856
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-15 11:55:09 -0300

    Ejercicio 4 If
    Ejercicio 4 resuelto

---
+++ jsInstruccionIF (4) Ejercicio 4 2.0.js
@@ -0,0 +1,12 @@
+function mostrar()
+{
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+	if(edad>12 && edad<18)
+		{
+			alert("Es adolescente");
+		}
+
+
+}//FIN DE LA FUNCIÓN
