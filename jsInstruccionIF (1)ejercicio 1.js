3d971ee46de8825c4ed2f6b58067b87dbaae7750
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-15 11:48:54 -0300

    Ejercicio 1 If
    If

---
+++ jsInstruccionIF (1)ejercicio 1.js
@@ -0,0 +1,15 @@
+function mostrar()
+{
+	
+if(numero)
+	{
+		alert("verdadero");
+	}
+	else
+	{
+		alert("falso");
+	}
+//0 o false= el alert me va a detectar como falso. Si pones 1 hasta mas infinito o -1 hasta menos infinito te va a dar verdadero. Si esta entre comillas "0" te da verdadero.
+
+
+}//FIN DE LA FUNCIÓN
