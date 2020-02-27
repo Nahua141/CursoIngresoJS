d2c6dbdb9dd053c80ebae43f9a9f42c70117607d
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-27 12:01:38 -0300

    Ejercicio 3 For

---
+++ jsIteracionesFor (3) ejercicio 3.js
@@ -0,0 +1,21 @@
+function mostrar()
+{
+
+	var repeticiones = prompt("ingrese el número de repeticiones");
+	var mensaje= "Hola UTN FRA";
+	
+
+	for(;;)
+	{
+		if(repeticiones>0)
+		{ 	
+		alert(mensaje);
+		
+		}
+
+	}
+
+
+
+}
+//FIN DE LA FUNCIÓN
