4c5d71503a661be155202e3ef50c2cb6e6d02495
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-27 12:00:23 -0300

    For

---
+++ jsIteracionesFor (1) ejercicio 1.js
@@ -0,0 +1,20 @@
+function mostrar()
+{
+	//for(;;)//estructura del for. Permite simplificar. Proceso de entrada y salidad de datos. For y While son lo mismo.
+	var contador;
+	
+	contador=0;
+
+	for(;;)
+	{
+		if(contador==10)
+		{
+			break;//para que pare de hacer una operacion.
+		}
+		console.log(contador);
+		contador++;
+	}
+	
+
+
+}
