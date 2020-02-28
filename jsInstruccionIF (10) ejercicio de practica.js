a422e909bfaedf753af838c9928d4b8a83938f3f
Nahua141 <60743520+Nahua141@users.noreply.github.com>
2020-02-28 10:04:45 -0300

    Ejercicio de practica

---
+++ jsInstruccionIF (10) ejercicio de practica.js
@@ -0,0 +1,45 @@
+function mostrar()
+//temperatura del mas joven y mas viejo
+// promedio de edad total, por mujeres.
+{ //si es niño, adolescente o mayor de edad, mostrar mensaje "Riesgo + nombre". 
+	var nombre;
+	var edad;
+	var sexo;
+	var temperatura;
+	var respuesta="Femenino";
+
+	temperatura=35;
+	edad>0
+
+	nombre=prompt("ingrese su nombre");
+	sexo=prompt("ingrese su sexo: Masculino o Femenino");
+	edad=prompt("ingrese su edad");
+	temperatura=prompt("Ingrese su temperatura");
+	
+	
+	if(temperatura>35)
+	{
+		alert("temperatura mas alta");
+	}	
+		else(temperatura<35)
+		{
+			alert("temperatura mas baja");
+		}
+		if(temperatura)
+		{
+			alert(" Esta en riego " + nombre);
+		}
+	
+
+	while(respuesta==Femenino)
+	{
+
+		edad++;
+	}
+	
+	prompt("desea continuar?");
+
+
+
+	
+}//FIN DE LA FUNCIÓN
